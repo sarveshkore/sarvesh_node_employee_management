d8f1ed3bfdadff6c9aeba601ef5f9992237905d2
sarveshkore <sarvesh2k2@gmail.com>
2024-10-20 19:36:23 +0530

    generated multiple files dynamically

--- src/file_system_practice/prac_generate_multiple_files_async.js
+++ src/file_system_practice/prac_generate_multiple_files_async.js
@@ -1,17 +1,22 @@
 function main(req,res){
     const fs=require("fs");
+    let folder = req.query.folderName;
+    let file = req.query.fileName;
+    let n=req.query.number;
     const data=req.body;
-    fs.mkdir('multiple_files',{recursive:true},()=>{
-        console.log('folder created')
-        for(let i=1;i<=5;i++){
-            // fs.writeFile('file'+`${i}`+'.txt',JSON.stringify(data),()=>{         //file created but not in the multiple_files directory.
-            fs.writeFile('multiple_files/file'+`${i}`+'.txt',JSON.stringify(data),()=>{
+    fs.mkdir(folder,{recursive:true},()=>{
+        console.log(`folder created : ${folder}`);
+        for(let i=1;i<=n;i++){
+            data["index"] = i;
+            // fs.writeFile(folder+'/'+file+`_${i}`+'.txt',JSON.stringify(data),()=>{
+                fs.writeFile(`${folder}/${file}_${i}.txt`,JSON.stringify(data),()=>{
 
-                console.log('file created');
+                console.log(`file created ${file}`);
                 console.log('data inserted :',data);
             })
         }
-    })
+        res.send(`folder created : ${folder}, file created : ${file}`);
+    });
 }
 
 module.exports={
