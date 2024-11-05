7d95f09f489d225edb3ef38d59ee836df99e115b
sarveshkore <sarvesh2k2@gmail.com>
2024-11-05 09:04:25 +0530

    aggregation_practice_2 ,

--- src/mongo_db_practice/prac_aggregation_function.js
+++ src/mongo_db_practice/prac_aggregation_function.js
@@ -11,37 +11,24 @@ async function main(req,res){
     const collection = db.collection('collection_practice');
 
     const agg= await collection.aggregate([
-        // {
-            
-        //     $match:{'student_detail.educational_details.percentage': {$gte:87 } }
-        // },
         {
-            $project:{
-                '_id':0,
-                // 'student_detail.s_id':1,
-                // 'student_detail.s_name':1,
-                // 'student_detail.educational_details.school_name':1,
-                // 'student_detail.educational_details.college_name':1,
-                // 'student_detail.educational_details.university_name':1,
-                // 'student_detail.educational_details.percentage':1,
-
-
-            }
+            $skip:1,
         },
-
         {
             $project: {
                 '_id':0,
-                'personalInfo.firstName':1,
-                'personalInfo.lastName':1,
-                'personalInfo.phone.mobile':1,
-                'academicDetails.courses.courseName':1,
-                'extracurricularActivities.activityName':1,
-                'awards.awardName':1,
-                'emergencyContact.contactPerson.relationship':1,
-                'emergencyContact.alternateContact.phone':1,
-
-            },
+                f_name:'$personalInfo.firstName',
+                l_name:'$personalInfo.lastName',
+                pointer:'$academicDetails.GPA',   //change academicDetails.GPA to pointer
+                // courseourseGrade: { $arrayElemAt: ["$academicDetails.courses.grade", 0] },
+                total_marks:{$sum:'$academicDetails.courses.marks'},
+                percentage: {
+                    $multiply: [
+                        { $divide: [{ $sum: '$academicDetails.courses.marks' }, 400] },
+                        100
+                    ]
+                }
+            }
         }
     ]).toArray();
 
