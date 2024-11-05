acd0fbf87ddf13f82d85836a2261c9e5185bd68b
sarveshkore <sarvesh2k2@gmail.com>
2024-11-05 12:31:59 +0530

    largest of percentage

--- src/mongo_db_practice/prac_aggregation_function.js
+++ src/mongo_db_practice/prac_aggregation_function.js
@@ -14,39 +14,72 @@ async function main(req,res){
         {
             $skip:1,
         },
-        {$unwind:'$academicDetails.courses'},
-        {
-            $project: {
-                '_id':0,
-                f_name:'$personalInfo.firstName',
-                l_name:'$personalInfo.lastName',
-                courseName:'$academicDetails.courses.courseName',
-                marks:'$academicDetails.courses.marks'
+
+        //get all student f_name, l_name, total_marks, percentage 
+        // {$unwind:'$academicDetails.courses'},
+        // {
+        //     $project: {
+        //         '_id':0,
+        //         f_name:'$personalInfo.firstName',
+        //         l_name:'$personalInfo.lastName',
+        //         courseName:'$academicDetails.courses.courseName',
+        //         marks:'$academicDetails.courses.marks'
                 
-            }
+        //     }
+        // },
+        // {
+        //     $group:{
+        //         _id: {
+        //             f_name: '$f_name', // Group by first name
+        //             l_name: '$l_name'  // Include last name in the group
+        //         },
+        //         total_marks:{$sum:'$marks'}
+        //     }
+        // },
+        // {
+        //     $project: {
+        //         _id: 0, 
+        //         f_name: '$_id.f_name', // Include the first name from the grouped _id
+        //         l_name: '$_id.l_name', // Include the last name from the grouped _id
+        //         total_marks: '$total_marks' ,
+        //         percentage: {
+        //             $multiply: [ { $divide: ['$total_marks', 400] }, 100 ]
+        //         }
+        //     }
+        // }
+
+        //largest percentage in group
+        {
+            $unwind: '$academicDetails.courses' // Flatten the courses array
         },
         {
-            $group:{
+            $group: {
                 _id: {
-                    f_name: '$f_name', // Group by first name
-                    l_name: '$l_name'  // Include last name in the group
+                    f_name: '$personalInfo.firstName', // Group by first name
+                    l_name: '$personalInfo.lastName'    // Group by last name
                 },
-                total_marks:{$sum:'$marks'}
+                total_marks: { $sum: '$academicDetails.courses.marks' } // Sum the marks
             }
         },
         {
             $project: {
-                _id: 0, 
-                f_name: '$_id.f_name', // Include the first name from the grouped _id
-                l_name: '$_id.l_name', // Include the last name from the grouped _id
-                total_marks: '$total_marks' ,
+                _id: 0, // Exclude the _id field
+                f_name: '$_id.f_name', // Include first name
+                l_name: '$_id.l_name', // Include last name
+                total_marks: '$total_marks', // Include total_marks
                 percentage: {
-                    $multiply: [ { $divide: ['$total_marks', 400] }, 100 ]
+                    $multiply: [{ $divide: ['$total_marks', 400] }, 100] // Calculate percentage
                 }
             }
+        },
+        {
+            // $sort: { percentage: 1 }//ASCENDING ORDER
+            $sort: { percentage: -1 }//DESCENDING ORDER
+
+        },
+        {
+            $limit:1,
         }
-
-
         
     ]).toArray();
 
